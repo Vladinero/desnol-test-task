1ec1b3c270dc2eb4a4a94c832b7f92eec85770d3
Vladinero <vlad.servicemail@gmail.com>
2024-07-06 16:06:51 +0300

    Added AppealFilter component

--- src/app/appeals/page.tsx
+++ src/app/appeals/page.tsx
@@ -1,10 +1,16 @@
+
+'use client';
 import Header from "@/components/Header/Header";
+import Container from "@mui/material/Container";
+import {AppealFilter} from "@/components/AppealsFilter/AppealsFilter";
 
 export default function Appeals() {
     return (
     <>
       <Header isAuth={true}/>
-       Appeals page
+       <Container maxWidth="xl" sx={{pt: 2}}>
+         <AppealFilter />
+       </Container>
     </>
     )
 }
---
+++ src/models/dataModel.ts
@@ -0,0 +1,15 @@
+export interface IDataModel {
+  id: number
+  user: string
+  theme: string
+  number: number
+  createdAt: string
+  modifiedAt: string
+  deadline: string
+  status: string
+  isWaitingForAnswer: string
+  description: string
+  decision: string
+  service: string
+  serviceContent: string
+}
